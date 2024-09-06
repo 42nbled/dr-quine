5153768c62ae9d9ff26b7fdcf42b593e897ef51a
KamiSama369 <nbled@DESKTOP-RJCT4R2>
2024-09-07 01:03:13 +0200

    Colleen and Grace

---
+++ Colleen/Colleen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Hello World !
+
+void	dr_quine(char *str, char tab, char nl, char dq) {
+	printf(str, str, tab, nl, dq);
+}
+
+int	main () {
+	char	*str = "#include <stdio.h>%3$c%3$c// Hello World !%3$c%3$cvoid%2$cdr_quine(char *str, char tab, char nl, char dq) {%3$c%2$cprintf(str, str, tab, nl, dq);%3$c}%3$c%3$cint%2$cmain () {%3$c%2$cchar%2$c*str = %4$c%1$s%4$c;%3$c%2$c// tab = 9, new_line = 10, double_quote = 34%3$c%2$cdr_quine(str, 9, 10, 34);%3$c%2$creturn (0);%3$c}%3$c";
+	// tab = 9, new_line = 10, double_quote = 34
+	dr_quine(str, 9, 10, 34);
+	return (0);
+}
---
+++ Grace/Grace.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Grace int main() {FILE *kid = fopen("Grace_kid.c", "w"); if (kid) {fprintf(kid, STR, STR, 10, 34);fclose(kid);}return (0);}
+#define STR "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$c#define Grace int main() {FILE *kid = fopen(%3$cGrace_kid.c%3$c, %3$cw%3$c); if (kid) {fprintf(kid, STR, STR, 10, 34);fclose(kid);}return (0);}%2$c#define STR %3$c%1$s%3$c%2$c%2$c// Hello World !%2$c%2$cGrace%2$c"
+
+// Hello World !
+
+Grace
