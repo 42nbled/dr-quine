c930486165345d80d7fe5c86c6a99275c3677a64
Nicolas BLED <nbled@made-f0Ar1s6.clusters.42paris.fr>
2024-09-13 22:33:25 +0200

    BONUS : CPP

---
+++ CPP/Colleen.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+	Hello World !
+*/
+
+void	dr_quine(const char *str, char tab, char nl, char dq) {
+	printf(str, str, tab, nl, dq);
+}
+
+int	main () {
+	const char	*str = "#include <stdio.h>%3$c%3$c/*%3$c%2$cHello World !%3$c*/%3$c%3$cvoid%2$cdr_quine(const char *str, char tab, char nl, char dq) {%3$c%2$cprintf(str, str, tab, nl, dq);%3$c}%3$c%3$cint%2$cmain () {%3$c%2$cconst char%2$c*str = %4$c%1$s%4$c;%3$c%2$c/*%3$c%2$c%2$ctab = 9, new_line = 10, double_quote = 34%3$c%2$c*/%3$c%2$cdr_quine(str, 9, 10, 34);%3$c%2$creturn (0);%3$c}%3$c";
+	/*
+		tab = 9, new_line = 10, double_quote = 34
+	*/
+	dr_quine(str, 9, 10, 34);
+	return (0);
+}
---
+++ CPP/Grace.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Grace int main() {FILE *kid = fopen("Grace_kid.cpp", "w"); if (kid) {fprintf(kid, STR, STR, 10, 34);fclose(kid);}return (0);}
+#define STR "#include <stdio.h>%2$c#include <stdlib.h>%2$c%2$c#define Grace int main() {FILE *kid = fopen(%3$cGrace_kid.cpp%3$c, %3$cw%3$c); if (kid) {fprintf(kid, STR, STR, 10, 34);fclose(kid);}return (0);}%2$c#define STR %3$c%1$s%3$c%2$c%2$c/*%2$c    Hello World !%2$c*/%2$c%2$cGrace%2$c"
+
+/*
+    Hello World !
+*/
+
+Grace
