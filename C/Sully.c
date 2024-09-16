917c27d57b050f4f7dfbb1511042464a63ffbe90
KamiSama369 <nbled@DESKTOP-RJCT4R2>
2024-09-16 09:56:06 +0200

    Sully.c shorter

--- C/Sully.c
+++ C/Sully.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STR "#include <stdio.h>%3$c#include <stdlib.h>%3$c%3$c#define STR %4$c%1$s%4$c%3$c%3$cint main() {%3$c%2$cint%2$ci = %5$d;%3$c%2$cchar%2$cfilename[11];%3$c%2$csprintf(filename, %4$cSully_%6$c%7$s.c%4$c, i - 1);%3$c%2$cFILE%2$c*kid = fopen(filename, %4$cw%4$c);%3$c%2$cif (kid) {%3$c%2$c%2$cfprintf(kid, STR, STR, 9, 10, 34, i - 1, 37, %4$c1$d%4$c);%3$c%2$c%2$cfclose(kid);%3$c%2$c%2$cif (i > 0) {%3$c%2$c%2$c%2$cchar%2$ccmd[62];%3$c%2$c%2$c%2$csprintf(cmd, %4$cclang -Wall -Wextra -Werror -o Sully_%6$c%7$s Sully_%6$c%7$s.c; ./Sully_%6$c%7$s%4$c, i - 1);%3$c%2$c%2$c%2$csystem(cmd);%3$c%2$c%2$c}%3$c%2$c}%3$c%2$creturn (0);%3$c}%3$c"
+#define STR "#include <stdio.h>%3$c#include <stdlib.h>%3$c%3$c#define STR %4$c%1$s%4$c%3$c%3$cint main() {%3$c%2$cint%2$ci = %5$d;%3$c%2$cchar%2$cfilename[11];%3$c%2$csprintf(filename, %4$cSully_%%1$d.c%4$c, i - 1);%3$c%2$cFILE%2$c*kid = fopen(filename, %4$cw%4$c);%3$c%2$cif (kid) {%3$c%2$c%2$cfprintf(kid, STR, STR, 9, 10, 34, i - 1);%3$c%2$c%2$cfclose(kid);%3$c%2$c%2$cif (i > 0) {%3$c%2$c%2$c%2$cchar%2$ccmd[62];%3$c%2$c%2$c%2$csprintf(cmd, %4$cclang -Wall -Wextra -Werror -o Sully_%%1$d Sully_%%1$d.c; ./Sully_%%1$d%4$c, i - 1);%3$c%2$c%2$c%2$csystem(cmd);%3$c%2$c%2$c}%3$c%2$c}%3$c%2$creturn (0);%3$c}%3$c"
 
 int main() {
 	int	i = 5;
@@ -9,7 +9,7 @@ int main() {
 	sprintf(filename, "Sully_%1$d.c", i - 1);
 	FILE	*kid = fopen(filename, "w");
 	if (kid) {
-		fprintf(kid, STR, STR, 9, 10, 34, i - 1, 37, "1$d");
+		fprintf(kid, STR, STR, 9, 10, 34, i - 1);
 		fclose(kid);
 		if (i > 0) {
 			char	cmd[62];
